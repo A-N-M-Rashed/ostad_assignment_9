e79aefcb6e9bafc4af2318f3c0617af545468787
A-N-M-Rashed <a.n.m.rashed045@gmail.com>
2026-06-19 00:31:53 +0600

    Question 1 Answered

---
+++ script.js
@@ -0,0 +1,14 @@
+//1. Select an element using:
+const heading = document.getElementById("heading").textContent;
+console.log("heading: "+ heading);
+
+const lastSkill=document.querySelector(".skills li:nth-child(3)");
+console.log("lastSkill: "+ lastSkill.textContent);
+
+
+const skills= document.querySelectorAll(".skills li");
+for(skill of skills){
+    console.log("List of skills:" + skill.textContent);
+}
+//End of Question 1
+
